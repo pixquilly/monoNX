3e24bdde3a4f56644daccd44fbe72e3d3e3a3633
pixquilly <pixquilly@gmail.com>
2025-10-28 21:32:10 +0200

    added lib-ui-table

---
+++ libs/ui/src/lib/table/table.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-table',
+  imports: [],
+  templateUrl: './table.html',
+  styleUrls: ['./table.scss', './table.less'],
+})
+export class Table {}
