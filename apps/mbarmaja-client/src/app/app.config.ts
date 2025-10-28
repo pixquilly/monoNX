0000f4d03b5d623b3f9d6240d42dd3a8a4fc3ccc
pixquilly <pixquilly@gmail.com>
2025-10-28 12:03:50 +0200

    added providers required by taiga ui

--- apps/mbarmaja-client/src/app/app.config.ts
+++ apps/mbarmaja-client/src/app/app.config.ts
@@ -6,10 +6,15 @@ import {
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideEventPlugins} from '@taiga-ui/event-plugins';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideAnimations(),
+    provideEventPlugins(),
   ],
 };
