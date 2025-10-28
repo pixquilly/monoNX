ddfaa8835bd2389aa52c408229b109abb997050f
pixquilly <pixquilly@gmail.com>
2025-10-28 15:33:32 +0200

    added an accordion component to ui lib

---
+++ libs/ui/src/lib/accordion/accordion.ts
@@ -0,0 +1,24 @@
+import {KeyValuePipe, NgForOf} from '@angular/common';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {TuiAccordion} from '@taiga-ui/experimental';
+
+@Component({
+  selector: 'lib-ui-accordion',
+  imports: [TuiAccordion, KeyValuePipe],
+  templateUrl: './accordion.html',
+  styleUrl: './accordion.scss',
+})
+export class Accordion {
+  
+  @Input() data: Record<string, string> = {
+    'Project Status Update': 
+      'The backend Express API setup is complete, and the database seeding is ready. We are currently integrating the Angular Task List component.',
+
+    'Next Steps for UI Library': 
+      'We need to define a theme switcher using Taiga UI tokens and finalize the style encapsulation settings across all components.',
+
+    'Why use NX Monorepo?': 
+      'NX provides better code sharing, consistent tooling (like generators), and a robust project graph for dependency analysis, improving build times.',
+  };
+}
+ 
--- libs/ui/src/lib/ui/ui.ts
+++ libs/ui/src/lib/ui/ui.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Button } from "../button/button";
 import {TuiRoot} from '@taiga-ui/core';
+import { Accordion } from "../accordion/accordion";
 @Component({
   selector: 'lib-ui',
-  imports: [TuiRoot, Button],
+  imports: [TuiRoot, Button, Accordion],
   templateUrl: './ui.html',
-  styleUrl: './ui.scss',
+  styleUrls: ['./ui.scss', './ui.less'],
 })
 export class UiLib {}
