66bd354b01deadbfac86177132759777167ca655
pixquilly <pixquilly@gmail.com>
2025-10-28 23:36:57 +0200

    updated table ui component

--- libs/ui/src/lib/table/table.ts
+++ libs/ui/src/lib/table/table.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
-
+import {AsyncPipe} from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TuiTable} from '@taiga-ui/addon-table';
+import {TuiFormatNumberPipe} from '@taiga-ui/core';
 @Component({
   selector: 'lib-table',
-  imports: [],
+  imports: [AsyncPipe, TuiFormatNumberPipe, TuiTable],
   templateUrl: './table.html',
   styleUrls: ['./table.scss', './table.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Table {}
+export class Table {
+  protected readonly data = [
+      {
+          name: 'Alex Inkin',
+          balance: 1323525,
+      },
+      {
+          name: 'Roman Sedov',
+          balance: 423242,
+      },
+  ] as const;
+
+  protected readonly columns = Object.keys(this.data[0]);
+}
